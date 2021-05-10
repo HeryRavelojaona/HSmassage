1259e9ca5ec0d66d9b03c5633fbc1a68ea0618d8
Hery Ravelojaona <hery.ravelojaona@gmail.com>
2021-05-10 15:24:43 +0200

    Init project

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>HS massage bien être</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Head>
+      <h1>Hello</h1>
+    </>
+    
+  )
+}
